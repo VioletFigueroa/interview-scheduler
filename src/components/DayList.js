5539705bf0ecdd37a7549261158950018e51c4be
Violet <VioletDFigueroa@gmail.com>
2021-05-12 19:29:28 -0700

    DayList and DayListItem components add

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+//import "components/DayList.scss";
+
+import classnames from "classnames";
+
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const days = props.days.map((day) => (
+    <DayListItem
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  ));
+
+  return <ul>{days}</ul>;
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+
+import classnames from "classnames";
+
+export default function DayListItem(props) {
+  const formatSpots = (spots) =>
+    spots === 1
+      ? `${spots} spot remaining`
+      : spots === 0
+      ? `no spots remaining`
+      : `${spots} spots remaining`;
+
+  const liClass = classnames("li", "day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  return (
+    <li className={liClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
