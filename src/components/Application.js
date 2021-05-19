8cb7b56c0b1d7694a5a0e4638646aee4c0b15d53
Violet <VioletDFigueroa@gmail.com>
2021-05-19 13:51:03 -0700

    Adds linting and minor bug fixes

--- src/components/Application.js
+++ src/components/Application.js
@@ -12,59 +12,58 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {
-      // 0: [
-      //   {
-      //     id: 1,
-      //     time: "12pm",
-      //   },
-      //   {
-      //     id: 2,
-      //     time: "1pm",
-      //     interview: {
-      //       student: "Lydia Miller-Jones",
-      //       interviewer: {
-      //         id: 1,
-      //         name: "Sylvia Palmer",
-      //         avatar: "https://i.imgur.com/LpaY82x.png",
-      //       },
-      //     },
-      //   },
-      //   {
-      //     id: 3,
-      //     time: "2pm",
-      //   },
-      //   {
-      //     id: 4,
-      //     time: "3pm",
-      //     interview: {
-      //       student: "Archie Cohen",
-      //       interviewer: {
-      //         id: 2,
-      //         name: "Tori Malcolm",
-      //         avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      //       },
-      //     },
-      //   },
-      //   {
-      //     id: 5,
-      //     time: "4pm",
-      //     interview: {
-      //       student: "Maria Boucher",
-      //       interviewer: {
-      //         id: 3,
-      //         name: "Mildred Nazir",
-      //         avatar: "https://i.imgur.com/T2WwVfS.png",
-      //       },
-      //     },
-      //   },
-      // ],
-    },
+    appointments: {},
+    // 0: [
+    //   {
+    //     id: 1,
+    //     time: "12pm",
+    //   },
+    //   {
+    //     id: 2,
+    //     time: "1pm",
+    //     interview: {
+    //       student: "Lydia Miller-Jones",
+    //       interviewer: {
+    //         id: 1,
+    //         name: "Sylvia Palmer",
+    //         avatar: "https://i.imgur.com/LpaY82x.png",
+    //       },
+    //     },
+    //   },
+    //   {
+    //     id: 3,
+    //     time: "2pm",
+    //   },
+    //   {
+    //     id: 4,
+    //     time: "3pm",
+    //     interview: {
+    //       student: "Archie Cohen",
+    //       interviewer: {
+    //         id: 2,
+    //         name: "Tori Malcolm",
+    //         avatar: "https://i.imgur.com/Nmx0Qxo.png",
+    //       },
+    //     },
+    //   },
+    //   {
+    //     id: 5,
+    //     time: "4pm",
+    //     interview: {
+    //       student: "Maria Boucher",
+    //       interviewer: {
+    //         id: 3,
+    //         name: "Mildred Nazir",
+    //         avatar: "https://i.imgur.com/T2WwVfS.png",
+    //       },
+    //     },
+    //   },
+    // ],
   });
   const setDay = (day) => setState({ ...state, day });
   useEffect(() => {
-    const api = '/api/'
-    const daysURL = "days";
+    const api = "/api/";
+    const daysURL = api + "days";
     const appointmentsURL = api + "appointments";
     // const interviewersURL = api + "interviewers";
     Promise.all([axios.get(daysURL), axios.get(appointmentsURL)])
