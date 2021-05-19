6a389c9129b53a56a7c56173aff84065be10cd41
Violet <VioletDFigueroa@gmail.com>
2021-05-19 00:06:02 -0700

    Adds getAppointmentsForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+import lodash from "lodash";
+
+export function getAppointmentsForDay(state, day) {
+  let output = [];
+  const filteredDays = state.days.filter((id) => id.name === day);
+  if (filteredDays.length === 0) {
+    return output;
+  }
+  const appointmentIDs = filteredDays[0]["appointments"];
+  for (const id of appointmentIDs) {
+    for (const key of Object.keys(state.appointments)) {
+      if (state.appointments[key]["id"] === id) {
+        output.push(state.appointments[key]);
+      }
+    }
+  }
+  return output;
+}
