7a096675e6a47f4546d0b2dda7869d5a70f2ca12
Violet <VioletDFigueroa@gmail.com>
2021-05-20 15:48:39 -0700

    Adds Arrow function edit

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,14 +3,14 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  function transition(newMode, replace = false) {
+  const transition = (newMode, replace = false) => {
     if (replace) {
       back();
     }
     setHistory((prev) => [...prev, newMode]);
     return setMode(newMode);
   }
-  function back() {
+  const back = () => {
     if (history.length > 1) {
       setHistory((prev) => [...prev.slice(0, history.length - 1)]);
       setMode(history[history.length - 2]);
