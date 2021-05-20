c93435036be98996ba7d66d8c72b4a708c52e193
Violet <VioletDFigueroa@gmail.com>
2021-05-20 15:37:58 -0700

    fixes TRANS(hehe)...ition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,15 +3,15 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  function transition(newMode) {
-    setHistory((prev) => [...prev, newMode]);
+  function transition(newMode, replace = false) {
+    if (!replace){
+      setHistory((prev) => [...prev, newMode]);
+    } 
     return setMode(newMode);
   }
   function back() {
     if (history.length > 1) {
-      console.log ("History at 12: " + history);
       setHistory((prev) => [...prev.slice(0,history.length-1)]);
-      console.log ("History at 14: " + history);
       setMode(history[history.length - 2]);
     }
   }
