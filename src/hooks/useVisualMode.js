9b9824980f2e13a87b4f7677b3c25a06ac2d3c1f
Violet <VioletDFigueroa@gmail.com>
2021-05-20 15:47:11 -0700

    Adds linting to TRANSition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,15 +4,15 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
   function transition(newMode, replace = false) {
-    if(replace) {
+    if (replace) {
       back();
     }
-      setHistory((prev) => [...prev, newMode]);
+    setHistory((prev) => [...prev, newMode]);
     return setMode(newMode);
   }
   function back() {
     if (history.length > 1) {
-      setHistory((prev) => [...prev.slice(0,history.length-1)]);
+      setHistory((prev) => [...prev.slice(0, history.length - 1)]);
       setMode(history[history.length - 2]);
     }
   }
