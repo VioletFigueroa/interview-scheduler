2182c2f3095155d675732eda76b196e6d5ef17c9
Violet <VioletDFigueroa@gmail.com>
2021-05-20 15:54:02 -0700

    Fixes hoisting issue

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,18 +3,16 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  const transition = (newMode, replace = false) => {
-    if (replace) {
-      back();
-    }
-    setHistory((prev) => [...prev, newMode]);
-    return setMode(newMode);
-  }
   const back = () => {
     if (history.length > 1) {
       setHistory((prev) => [...prev.slice(0, history.length - 1)]);
       setMode(history[history.length - 2]);
     }
-  }
+  };
+  const transition = (newMode, replace = false) => {
+    if (replace) back();
+    setHistory((prev) => [...prev, newMode]);
+    return setMode(newMode);
+  };
   return { mode, transition, back };
 }
